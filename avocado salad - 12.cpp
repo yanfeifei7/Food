660dfc99dd75d4c8192eac13fcc2fce080b59578
yanfeifei7 <andyduoduo6689659820091025@gmail.com>
2024-08-14 12:19:39 -0300

    Delete avocado salad - 12.cpp

--- avocado salad - 12.cpp
+++
@@ -1,9 +0,0 @@
-#include <bits/stdc++.h>
-using namespace std;
-int totaltime = 15;
-string ingredients[7] = {"Avocados", "Onions", "Bell pepper", "Tomato", "Cilantro", "Lime", "Salt + Pepper"};
-int main() {
-    cout << "Total time: " << totaltime << " minutes\nIngredients:\n";
-    for (int i = 0; i < 7; i++) cout << i + 1 << ". " << ingredients[i] << "\n";
-    return 0;
-}
