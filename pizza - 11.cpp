6aa66dd7a020a0c0b70398e2b2ffe23da9213267
yanfeifei7 <andyduoduo6689659820091025@gmail.com>
2024-08-14 12:19:31 -0300

    Delete pizza - 11.cpp

--- pizza - 11.cpp
+++
@@ -1,9 +0,0 @@
-#include <bits/stdc++.h>
-using namespace std;
-int totaltime = 2.5;
-string ingredients[7] = {"Yeast", "Water", "Flour", "Oil", "Sugar", "Salt", "Cornmeal"};
-int main() {
-    cout << "Total time: " << totaltime << " hours\nIngredients:\n";
-    for (int i = 0; i < 7; i++) cout << i + 1 << ". " << ingredients[i] << "\n";
-    return 0;
-}
